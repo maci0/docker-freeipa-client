dbc3203810934ef856a1f4fae660df62b4c1a778
Marcel Wysocki <maci.stgn@gmail.com>
2015-01-13 16:55:46 +0100

    more workarounds

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-ipa-client-install -U --server=ipa.example.com --domain=example.com -p admin -w changeme --mkhomedir
+#!/bin/sh
+PASSWORD="changeme"
+REALM="example.com"
+
+#workaround
+mv /usr/bin/domainname /usr/bin/domainname.bak
+echo '#!/bin/sh
+exit 0' > /usr/bin/domainname
+chmod +x /usr/bin/domainname
+
+ipa-client-install -U --server=ipa."${REALM}" --domain="${REALM}" -p admin -w "${PASSWORD}" --mkhomedir
